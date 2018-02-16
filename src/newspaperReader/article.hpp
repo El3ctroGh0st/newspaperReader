84dabd82832390347196a3d8de346545407f0f0b
El3ctroGh0st <ldst1050@hotmail.com>
2018-02-16 15:14:44 +0100

    Added read bool for later feature

--- src/newspaperReader/article.hpp
+++ src/newspaperReader/article.hpp
@@ -9,19 +9,21 @@ class Article
 {
 public:
     Article() = default;
-    Article(QString title, QString pubDate, QUrl linkAddress, QString newspaper) : title(title), pubDate(pubDate), linkAddress(linkAddress), newspaper(newspaper) {tags << newspaper << "All";}
+    Article(QString title, QString pubDate, QUrl linkAddress, QString newspaper) : title(title), pubDate(pubDate), linkAddress(linkAddress), newspaper(newspaper), read(false) {tags << newspaper << "All";}
 
     void setTitle (const QString& title) {this->title = title;}
     void setPubDate(const QString& pubDate) {this->pubDate = pubDate;}
     void setNewspaper (const QString &newspaper) {this->newspaper = newspaper;}
     void setLinkAddress(const QUrl& linkAddress) {this->linkAddress = linkAddress;}
     void addTag(const QString& tag) {this->tags << tag;}
+    void setRead(const bool read) {this->read = read;}
 
     QString getTitle() const {return title;}
     QString getPubDate() const {return pubDate;}
     QString getNewspaper() const {return newspaper;}
     QUrl getLinkAddress() const {return linkAddress;}
     QStringList getTags() const {return tags;}
+    bool getRead() const {return read;}
 
 private:
     QString title;
@@ -29,6 +31,7 @@ private:
     QUrl linkAddress;
     QString newspaper;
     QStringList tags;
+    bool read;
 };
 
 #endif // ARTICLE_HPP
